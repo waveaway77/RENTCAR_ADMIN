6076af32b011830a266d2eeb514148462a181fba
waveaway77 <waveaway77@gmail.com>
2025-01-27 20:40:02 +0900

    init and 카테고리 조회 완료

--- src/main/java/org/waveaway77/rentcar_admin/category/controller/GetCategoryController.java
+++ src/main/java/org/waveaway77/rentcar_admin/category/controller/GetCategoryController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.waveaway77.rentcar_admin.category.dto.GetCategoryResponse;
 import org.waveaway77.rentcar_admin.category.repository.CategoryRepository;
 
+/**
+ * 작성자 : 이지예
+ * 작성일자 : 2025.01.27
+ * 전문정의 : admin 페이지의 카테고리 목록을 리턴한다
+ */
 @RestController
 public class GetCategoryController {
 
@@ -19,6 +24,7 @@ public class GetCategoryController {
         GetCategoryResponse response = new GetCategoryResponse();
         response.setCategoryArray(repository.findCategory());
 
+        /* 2. return response */
         return response;
     }
 
