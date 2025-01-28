e945250d131983d7bd0422d6a8095c2bb4ff38d5
waveaway77 <waveaway77@gmail.com>
2025-01-28 12:14:16 +0900

    prettier

--- src/main/java/org/waveaway77/rentcar_admin/car/dto/FixCarStatusResponse.java
+++ src/main/java/org/waveaway77/rentcar_admin/car/dto/FixCarStatusResponse.java
@@ -1,10 +1,8 @@
 package org.waveaway77.rentcar_admin.car.dto;
 
-import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
-@Getter
 @Setter
 public class FixCarStatusResponse {
     private HttpStatus status;
